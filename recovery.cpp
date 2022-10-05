3a23232acfd52ad823c730f435744d97ef084908
mukesh22584 <mks22584@gmail.com>
2022-10-06 01:58:06 +0530

    recovery: Get proper Xtended version

--- recovery.cpp
+++ recovery.cpp
@@ -824,7 +824,7 @@ Device::BuiltinAction start_recovery(Device* device, const std::vector<std::stri
   std::string ver_date = ver_date_match.str(1);  // Empty if no match.
 
   std::vector<std::string> title_lines = {
-    "Version " + android::base::GetProperty("ro.xtended.display.version", "(unknown)") +
+    "Version " + android::base::GetProperty("ro.xtended.build.version", "(unknown)") +
         "-" + ver_date,
   };
   if (android::base::GetBoolProperty("ro.build.ab_update", false)) {
